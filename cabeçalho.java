0b25e34ba173127d3e94accbc36beb605e674f91
Pitaluginha <123636237+Pitaluginha@users.noreply.github.com>
2025-05-27 20:16:10 -0300

    Create cabeçalho.java

---
+++ cabeçalho.java
@@ -0,0 +1,10 @@
+public class Cabecalho {
+    public static void exibir() {
+        System.out.println("======================================");
+        System.out.println(" Faculdade: Nome da Faculdade ");
+        System.out.println(" Aluno: Seu Nome ");
+        System.out.println(" Professor: Nome do Professor ");
+        System.out.println(" Tema: Quiz de Conhecimento Geral ");
+        System.out.println("======================================\n");
+    }
+}
